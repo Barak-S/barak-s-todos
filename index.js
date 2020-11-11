e1394f27974728ed61616af9dd707aeb98b07e4c
Barak-S <barak.saidoff@gmail.com>
2020-11-11 10:10:07 -0500

    added filter dropdown

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const todo = document.querySelector('.todo-input');
     const todoButton = document.querySelector('.todo-button');
     const todoList = document.querySelector('.todo-list');
+    const filterOption = document.querySelector('select')
 
 
     // event listeners
     todoButton.addEventListener('click', addTodo)
     todoList.addEventListener('click', deleteCheck)
+    filterOption.addEventListener('click', filterTodos)
 
 
     // functions
@@ -46,6 +48,33 @@ document.addEventListener('DOMContentLoaded', () => {
             const toDo = item.parentNode
             toDo.classList.toggle('completed')
         }
+    }
+
+    function filterTodos(e){
+        console.log(e.target.value)
+        const todos = todoList.children
+        var arr = Array.prototype.slice.call( todos )
+        arr.map((todo)=>{
+            switch(e.target.value){
+                case "all":
+                    todo.style.display = 'flex'
+                    break;
+                case 'completed':
+                    if(todo.classList.contains('completed')){
+                        todo.style.display = 'flex'
+                    } else{
+                        todo.style.display = 'none'
+                    }
+                    break;
+                case 'uncompleted':
+                    if(!todo.classList.contains('completed')){
+                        todo.style.display = 'flex'
+                    } else{
+                        todo.style.display = 'none'
+                    }
+                    break;
+            }
+        })
     }
     
 
